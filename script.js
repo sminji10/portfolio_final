00fa65c4f2e42838be2f0ed343b8449c607fceb5
Minji Son <alswml10@naver.com>
2023-02-08 11:20:05 +0900

    third commit

--- script.js
+++ script.js
@@ -44,9 +44,49 @@
         },
       });
 
+//썸네일을 눌렀을 때 팝업창이 등장하는 함수&이미지 주소 연결
+const thumbnails = document.querySelectorAll(".box_gallery > li");
+const overlay = document.querySelector(".slide-overlay");
+let slidePhoto = document.querySelectorAll(".slides > li>img");
 
-//click popup
-      var img = document.getElementsByClassName('click_img_1');
-      for (var x = 0; x < img.length; x++){
-        img.item(x).onclick=function() {window.open(this.src)};
-      }
+thumbnails.forEach((thumbnail) => {
+  thumbnail.addEventListener("click", (e) => {
+    e.preventDefault();
+    overlay.style.display = "block";
+    // 썸네일 원본 사진과 갤러리 슬라이드 이미지 소스 링크 연결
+    for (let i = 0; i < thumbnails.length; i++) {
+      let photo = thumbnails[i].lastElementChild;
+      slidePhoto[i].src = photo.href;
+    }
+  });
+});
+document.querySelector(".close-btn").addEventListener("click", () => {
+  overlay.style.display = "none";
+});
+
+/*메뉴 클릭시 이동
+$(".menu>li").click(function(){
+  click_num=$(this).index();
+  cnt=click_num;
+  page_y=$(".scroll").eq(click_num).offset().top;
+  $("html").animate({scrollTop:page_y},400);
+ })
+ */
+
+
+//scrollTop
+  //조건1. top을 클릭했을 때 
+  $(".topBtn").click(function(e){
+    e.preventDefault(); //a태그의 효과제거(제일상단으로 이동되는 효과->태그에 기본적으로 있는 이벤트 효과 제거)
+    $("html").animate({scrollTop:0},500)
+   });
+   //조건2. top을 클릭했을 때 천천히 시간을 줘서 상단으로 이동
+   
+   $(window).scroll(function(){
+    if($("html").scrollTop()>100){//html의 상단top을 기준으로 스크롤이 100보다 클 때
+       $(".topBtn").fadeIn(100)//.top이 나타난다.
+       }
+    else{
+       $(".topBtn").fadeOut(100)//.top이 사라진다.
+       }   
+   });
